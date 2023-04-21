0d202b76cf6ec895b2b7c48eade921644ef6f967
Susobhan Dash <Susobhan.Dash@efi.com>
2023-04-21 17:11:15 +0530

    Initial Commit
    Added style files,
    Base HTML wireframe
    Added Font Awesome and Gothic font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'susobhan-profile';
+  title = 'Susobhan Dash Resume';
 }
